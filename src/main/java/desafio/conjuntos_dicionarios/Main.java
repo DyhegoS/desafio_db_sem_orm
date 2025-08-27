2588decd100ca110f35e9dc1890b9935046723a5
DyhegoS <dyhegodasilva@gmail.com>
2025-08-27 18:01:56 -0300

    emoção e adrenalina

---
+++ src/main/java/desafio/conjuntos_dicionarios/Main.java
@@ -0,0 +1,65 @@
+package desafio.conjuntos_dicionarios;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import desafio.conjuntos_dicionarios.entities.Department;
+import desafio.conjuntos_dicionarios.entities.Employee;
+
+public class Main {
+    public static void main(String[] args) {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            File entrys = new File("C:\\temp\\jsons");
+
+            File[] jsonData = entrys.listFiles(File::isFile);
+
+            for(File json : jsonData){
+                String[] record = objectMapper.readValue(json, new TypeReference<String[]>() {});
+                convertRecords(record);
+                System.out.println("====================================================================");
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    static List<Department> convertRecords(String[] records) {
+        Map<Long, String> dict = new HashMap<>();
+        Department dp = new Department();
+        List<Department> result = new ArrayList<>();
+        
+        for(String rec : records){
+            String[] parts = rec.split(",");
+            String name = parts[3];
+            Long id = Long.parseLong(parts[2]);
+            Double salary = Double.parseDouble(parts[4]);
+            Long departmentId = Long.parseLong(parts[0]);
+            String departmentName = parts[1];
+
+            if(!dict.containsKey(departmentId)){
+                dict.put(departmentId, departmentName);
+            }
+
+            for(Long d : dict.keySet()){
+                if(departmentName == dict.get(d)){
+                    dp.addEmployee(new Employee(id, name, salary, new Department(departmentId, departmentName)));
+                }
+            }
+              
+        }     
+    
+        return null;
+    }
+}
---
+++ src/main/java/desafio/conjuntos_dicionarios/entities/Department.java
@@ -0,0 +1,48 @@
+package desafio.conjuntos_dicionarios.entities;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Department {
+    private Long id;
+    private String name;
+    private Set<Employee> emp = new HashSet<>();
+
+    public Department(){
+    }
+    
+    public Department(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Employee> getEmp() {
+        return emp;
+    }
+
+    public void addEmployee(Employee emp){
+        this.emp.add(emp);
+    }
+
+    @Override
+    public String toString() {
+        return name + ": " + getEmp();
+    }
+    
+}
